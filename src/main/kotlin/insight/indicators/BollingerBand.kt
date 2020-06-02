3d7c081557bcdfda0d70e9e07e65ca2b6f07c209
Pablo Pallocchi <pablopallocchi@gmail.com>
2020-06-01 23:51:28 -0300

    Add Bollinger Bands indicator

---
+++ src/main/kotlin/insight/indicators/BollingerBand.kt
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Pablo Pallocchi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package insight.indicators
+
+import insight.series.Series
+
+/**
+ * The Bollinger Bands, defined as two standard deviations (positively and negatively) away from the [sma].
+ *
+ * Returns a triple of middle, upper and lower lines.
+ *
+ * **See:** [TradingView](https://www.tradingview.com/pine-script-reference/#fun_bb)
+ *
+ * @param source Series of values to process
+ * @param n Number of bars (length)
+ * @param factor Standard deviation factor
+ * @sample insight.indicators.BollingerBandTest.withIntSeries
+ * @see [sma] [stdev]
+ */
+fun bb(
+    source: Series,
+    n: Int = 20,
+    factor: Int = 2
+): Triple<Series, Series, Series> {
+    val basis = sma(source, n)
+    val dev = stdev(source, n) * factor
+    val upper = basis + dev
+    val lower = basis - dev
+    return Triple(basis, upper, lower)
+}
